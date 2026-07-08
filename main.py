f5c84997bd89afddd28c5b66c55020cc9f9a5ab3
black <black@example.com>
2026-07-08 15:03:03 +0900

    All greeting functions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from hello_app.greet import say_hello
+from hello_app.greet import say_hello, greetWhite, greetBlack
 
 
 def main():
     say_hello()
+    greetWhite()
+    greetBlack()
 
 
 if __name__ == "__main__":
